5cffaab4a6918f5fff09fee4705a8cf4b343e62b
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-03-30 23:52:02 +0200

    Add files via upload

--- src/main/java/de/klenze_kk/lingling/Main.java
+++ src/main/java/de/klenze_kk/lingling/Main.java
@@ -8,27 +8,28 @@ import de.klenze_kk.jingling.Gui.*;
 public class Main {
 	static JFrame WINDOW;
 	static JPanel PANEL;
+
 	public static void main(String[] args) {
 		initWin();
-		
+
 	}
 
 	private static void initWin() {
 		WINDOW = new JFrame("你 行!");
-		WINDOW.setSize(800,600);
+		WINDOW.setSize(800, 600);
 		WINDOW.setResizable(false);
 		WINDOW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		WINDOW.setLocationRelativeTo(null);
 		PANEL = new LogScreen();
-		
-		
+
 		WINDOW.add(PANEL);
 		WINDOW.setVisible(true);
 	}
+
 	public static void setJPanel(JPanel p) {
 		WINDOW.remove(PANEL);
 		PANEL = p;
 		WINDOW.add(PANEL);
 	}
-	
+
 }
