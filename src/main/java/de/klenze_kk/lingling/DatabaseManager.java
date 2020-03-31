f340c3baac56fb8b8654afb9d6ad7a433f0fa255
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-03-31 22:26:26 +0200

    Update DatabaseManager.java

--- src/main/java/de/klenze_kk/lingling/DatabaseManager.java
+++ src/main/java/de/klenze_kk/lingling/DatabaseManager.java
@@ -5,6 +5,8 @@ import java.util.*;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 
+import de.klenze_kk.lingling.logic.StatisticKey;
+import de.klenze_kk.lingling.logic.User;
 import de.klenze_kk.lingling.logic.Vocabulary;
 
 public final class DatabaseManager {
@@ -12,8 +14,8 @@ public final class DatabaseManager {
     private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
     private static final String URL_PREFIX = "jdbc:mysql://";
 
-    protected final String host, database, userName, password;
-    protected final short port;
+    private final String host, database, userName, password;
+    private final short port;
 
     protected Connection connection;
 
@@ -40,7 +42,7 @@ public final class DatabaseManager {
     }
 
     public void loadVocabulary(Consumer<Set<Vocabulary>> consumer) {
-        new Thread(new VocabularyLoder(consumer)).start();
+        new Thread(new VocabularyLoader(consumer)).start();
     }
 
     private static final String VOCABULARY_QUERY = "SELECT * FROM Vocabulary;";
@@ -51,15 +53,15 @@ public final class DatabaseManager {
     private static final String TERM_COLUMN = "Term";
     private static final String PAGE_NUMBER_COLUMN = "Page_Number";
 
-    private final class VocabularyLoder implements Runnable {
+    private final class VocabularyLoader implements Runnable {
 
         private final Consumer<Set<Vocabulary>> consumer;
 
-        protected VocabularyLoder(Consumer<Set<Vocabulary>> consumer) {
+        protected VocabularyLoader(Consumer<Set<Vocabulary>> consumer) {
             this.consumer = consumer;
         }
 
-        public synchronized void run() {
+        public void run() {
             final Set<Vocabulary> vocabulary = new HashSet<Vocabulary>();
 
             try {
@@ -70,14 +72,14 @@ public final class DatabaseManager {
                     final String chinese = table.getString(CHINESE_COLUMN);
                     if(chinese == null) continue;
 
+                    final String translation = table.getString(TRANSLATION_COLUMN);
+                    if(translation == null) continue;
+
                     String pinyin = table.getString(PINYIN_COLUMN);
                     if(pinyin == null) pinyin = "";
 
                     String rawPinyin = table.getString(RAW_PINYIN_COLUMN);
                     if(rawPinyin == null) rawPinyin = pinyin;
-
-                    final String translation = table.getString(TRANSLATION_COLUMN);
-                    if(translation == null) continue;
 
                     String term = table.getString(TERM_COLUMN);
                     if(term == null) term = "";
@@ -93,6 +95,62 @@ public final class DatabaseManager {
             }
 
             consumer.accept(vocabulary);
+        }
+
+    }
+
+    private static final String USER_COLUMN = "Username";
+    private static final String PASSWORD_COLUMN = "Password";
+    private static final String USERNAME_VAR = "%userName%";
+    private static final String LOGIN_QUERY;
+    static {
+        LOGIN_QUERY = new StringBuilder("SELECT * FROM User WHERE ")
+                        .append(USER_COLUMN).append(" = '").append(USERNAME_VAR).append("';").toString();
+    }
+
+    
+    private static final StatisticKey[] STAT_KEYS = StatisticKey.values();
+
+    public void performLogin(Consumer<LoginResponse> consumer, String userName, String password) {
+        new Thread(new LoginTask(consumer, userName, password)).start();
+    }
+
+    private final class LoginTask implements Runnable {
+
+        private final Consumer<LoginResponse> consumer;
+        private final String name;
+        private final String password;
+
+        protected LoginTask(Consumer<LoginResponse> consumer, String name, String password) {
+            this.consumer = consumer;
+            this.name = name;
+            this.password = password;
+        }
+
+        public void run() {
+            final LoginResponse response;
+            try {
+                openConnection();
+
+                final ResultSet data = connection.createStatement().executeQuery(LOGIN_QUERY.replaceAll(USERNAME_VAR, name));
+                if(data.next()) {
+                    if(password.equals(data.getString(password))) {
+
+                        final EnumMap<StatisticKey,Integer> stats = new EnumMap<StatisticKey,Integer>(StatisticKey.class);
+                        // load statistics from ResultSet object
+
+                        response = new LoginResponse.SuccessfulLogin(new User(name, stats));
+                    }
+                    else response = LoginResponse.FailedLogin.WRONG_PASSWORD;
+                }
+                else response = LoginResponse.FailedLogin.USER_UNKNOWN;
+            }
+            catch (Exception ex) {
+                Main.log(Level.SEVERE, "Failed to check login data", ex);
+                return;
+            }
+
+            consumer.accept(response);
         }
 
     }
