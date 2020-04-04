cbb2f5408405a326ad6b284c21df773b05a575ee
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-04-05 00:03:24 +0200

    Update Main.java

--- src/main/java/de/klenze_kk/lingling/Main.java
+++ src/main/java/de/klenze_kk/lingling/Main.java
@@ -4,18 +4,23 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
-import de.klenze_kk.jingling.Gui.*;
+import de.klenze_kk.lingling.Gui.*;
 
 public class Main {
 	private static final Logger LOGGER = Logger.getLogger("[Lingling-System]");
 	private static JFrame WINDOW;
 	private static JPanel panel;
+	private static DatabaseManager DATABASE_MANAGER;
+	
 	
 	public static void main(String[] args) {
 		initWin();
-
+		DATABASE_MANAGER = new DatabaseManager(null, (short) 0, null, null, null);
+		handleError("DU BLÖDIAN DEIN PROGRAM IST KAPUTT", true);
 	}
 
 	private static void initWin() {
@@ -30,9 +35,11 @@ public class Main {
 		WINDOW.setVisible(true);
 	}
 
-	public static void setJPanel(JPanel p) {
+	public static synchronized void setJPanel(JPanel p) {
+		panel.setVisible(false);
 		WINDOW.remove(panel);
 		panel = p;
+		panel.setVisible(true);
 		WINDOW.add(panel);
 	}
 	
@@ -47,5 +54,24 @@ public class Main {
 	            LOGGER.log(logLevel, message, error);
 	        }
 	    }
-
+	  
+	   public static DatabaseManager getDatabaseManager() {
+			return DATABASE_MANAGER;
+		}
+	   
+	   public static void handleError(String message, boolean closeProgram) {
+		   JFrame jf = new JFrame("Error");
+		   jf.setSize(500, 100);
+		   jf.setDefaultCloseOperation(closeProgram ? JFrame.EXIT_ON_CLOSE : JFrame.DISPOSE_ON_CLOSE);
+		   if(closeProgram) WINDOW.setVisible(false); 
+		   jf.setResizable(false);
+		   
+		   JLabel jl = new JLabel((closeProgram ? "Program crashed: " : "Error occurred: ")+message,SwingConstants.CENTER);
+		   jf.add(jl);
+		   jf.setLocationRelativeTo(null);
+		   jl.setVisible(true); 
+		   jf.setVisible(true);  
+		   
+	   }
+	   
 }
