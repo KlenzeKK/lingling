8ad3d43df92dd7c7e0d838acb57a07b66ad8d2b7
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-04-10 23:01:04 +0200

    Create VocabularyManager.java

---
+++ src/main/java/de/klenze_kk/lingling/logic/VocabularyManager.java
@@ -0,0 +1,61 @@
+package de.klenze_kk.lingling.logic;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public final class VocabularyManager implements Consumer<Set<Vocabulary>> {
+
+    private final Set<Vocabulary> vocabulary = new HashSet<Vocabulary>();
+    private final Map<Short,Set<Vocabulary>> pagesCache = new HashMap<Short,Set<Vocabulary>>();
+
+    public synchronized void accept(Set<Vocabulary> vocs) {
+        vocabulary.clear();
+        pagesCache.clear();
+
+        vocabulary.addAll(vocs);
+
+        Set<Vocabulary> currentPage;
+        for(Vocabulary voc: vocs) {
+            currentPage = pagesCache.get(voc.pageNumber);
+            if(currentPage == null) {
+                currentPage = new HashSet<Vocabulary>();
+                pagesCache.put(voc.pageNumber, currentPage);
+            }
+
+            currentPage.add(voc);
+        }
+    }
+
+    public synchronized Set<Vocabulary> getVocabualary() {
+        return new HashSet<Vocabulary>(vocabulary);
+    }
+
+    public synchronized Set<Vocabulary> performQuery(String query, Set<Short> pageNumbers, Set<String> terms) {
+        final Set<Vocabulary> queryResult = new HashSet<Vocabulary>();
+
+        query = query.toLowerCase();
+
+        for(Short s: pageNumbers) {
+            for(Vocabulary voc: pagesCache.get(s)) {
+                if(!terms.contains(voc.term) ||
+                   !voc.chinese.contains(query) ||
+                   !voc.rawPinyin.toLowerCase().contains(query) ||
+                   !voc.translation.toLowerCase().contains(query)) continue;
+
+                queryResult.add(voc);
+            }
+        }
+
+        return queryResult;
+    }
+
+    public boolean correctTranslation(String chinese, String german) {
+        for(Vocabulary voc: vocabulary) {
+            if(voc.chinese.equals(chinese) && voc.translation.equalsIgnoreCase(german))
+                return true;
+        }
+
+        return false;
+    }
+
+}
