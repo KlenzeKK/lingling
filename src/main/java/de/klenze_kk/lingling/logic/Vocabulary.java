952ce471ef22355c04bc047fa547d40ff0be2ec5
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-04-12 22:14:37 +0200

    Update Vocabulary.java

--- src/main/java/de/klenze_kk/lingling/logic/Vocabulary.java
+++ src/main/java/de/klenze_kk/lingling/logic/Vocabulary.java
@@ -1,17 +1,25 @@
 package de.klenze_kk.lingling.logic;
 
+import javax.swing.ImageIcon;
+
 public final class Vocabulary {
 
     public final String chinese, pinyin, rawPinyin, translation, term;
     public final short pageNumber;
+    private final byte[] gif;
 
-    public Vocabulary(String chinese, String pinyin, String rawPinyin, String translation, String term, short pageNumber) {
+    public Vocabulary(String chinese, String pinyin, String rawPinyin, String translation, String term, short pageNumber, byte[] gif) {
         this.chinese = chinese;
         this.pinyin = pinyin;
         this.rawPinyin = rawPinyin;
         this.translation = translation;
         this.term = term;
         this.pageNumber = pageNumber;
+        this.gif = gif;
+    }
+
+    public ImageIcon getGif() {
+        return new ImageIcon(gif);
     }
 
 }
