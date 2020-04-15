f7fc361a6225eba867f3c75e8f254e8ef3db11a5
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-04-15 23:30:12 +0200

    Update Vocabulary.java

--- src/main/java/de/klenze_kk/lingling/logic/Vocabulary.java
+++ src/main/java/de/klenze_kk/lingling/logic/Vocabulary.java
@@ -1,16 +1,15 @@
 package de.klenze_kk.lingling.logic;
 
+import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public final class Vocabulary {
 
-    public final int id;
-    public final short pageNumber;
     public final String chinese, pinyin, rawPinyin, translation, term;
+    public final short pageNumber;
     private final byte[] gif;
 
-    public Vocabulary(int id, String chinese, String pinyin, String rawPinyin, String translation, String term, short pageNumber, byte[] gif) {
-        this.id = id;
+    public Vocabulary(String chinese, String pinyin, String rawPinyin, String translation, String term, short pageNumber, byte[] gif) {
         this.chinese = chinese;
         this.pinyin = pinyin;
         this.rawPinyin = rawPinyin;
@@ -20,13 +19,10 @@ public final class Vocabulary {
         this.gif = gif;
     }
 
-    public ImageIcon getGif() {
-        return new ImageIcon(gif);
+    public ImageIcon getGif(int groeße) {
+        return new ImageIcon(new ImageIcon(gif).getImage().getScaledInstance(groeße, groeße, Image.SCALE_DEFAULT));
     }
-
-    @Override
-    public boolean equals(Object anObject) {
-        return anObject instanceof Vocabulary ? ((Vocabulary) anObject).id == this.id : false;
-    }
+    
+    
 
 }
