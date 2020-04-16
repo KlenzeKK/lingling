ea3ec8e9593951add80a6e2dd0d5784de9db89d6
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-04-17 00:18:04 +0200

    Update Vocabulary.java

--- src/main/java/de/klenze_kk/lingling/logic/Vocabulary.java
+++ src/main/java/de/klenze_kk/lingling/logic/Vocabulary.java
@@ -1,15 +1,18 @@
 package de.klenze_kk.lingling.logic;
 
 import java.awt.Image;
+
 import javax.swing.ImageIcon;
 
 public final class Vocabulary {
 
-    public final String chinese, pinyin, rawPinyin, translation, term;
+    public final int id;
     public final short pageNumber;
+    public final String chinese, pinyin, rawPinyin, translation, term;
     private final byte[] gif;
 
-    public Vocabulary(String chinese, String pinyin, String rawPinyin, String translation, String term, short pageNumber, byte[] gif) {
+    public Vocabulary(int id, String chinese, String pinyin, String rawPinyin, String translation, String term, short pageNumber, byte[] gif) {
+        this.id = id;
         this.chinese = chinese;
         this.pinyin = pinyin;
         this.rawPinyin = rawPinyin;
@@ -19,10 +22,13 @@ public final class Vocabulary {
         this.gif = gif;
     }
 
-    public ImageIcon getGif(int groeße) {
-        return new ImageIcon(new ImageIcon(gif).getImage().getScaledInstance(groeße, groeße, Image.SCALE_DEFAULT));
+    public ImageIcon getGif(int pixel) {
+        return new ImageIcon(new ImageIcon(gif).getImage().getScaledInstance(pixel * chinese.length(), pixel, Image.SCALE_DEFAULT));
     }
-    
-    
+
+    @Override
+    public boolean equals(Object anObject) {
+        return anObject instanceof Vocabulary ? ((Vocabulary) anObject).id == this.id : false;
+    }
 
 }
