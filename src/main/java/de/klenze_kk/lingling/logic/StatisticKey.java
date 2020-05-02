eb27ea7a115d152788b2341d9bdb9e0f0582a67e
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-05-02 23:58:10 +0200

    Update StatisticKey.java

--- src/main/java/de/klenze_kk/lingling/logic/StatisticKey.java
+++ src/main/java/de/klenze_kk/lingling/logic/StatisticKey.java
@@ -1,15 +1,40 @@
 package de.klenze_kk.lingling.logic;
 
-public enum StatisticKey {
+import java.util.EnumMap;
+import java.util.Map;
 
-    ;
+import de.klenze_kk.lingling.Main;
 
-    public final String databaseColumn;
-    public final boolean theHigherTheBetter;
+public final class User {
 
-    private StatisticKey(String column, boolean theHigherTheBetter) {
-        this.databaseColumn = column;
-        this.theHigherTheBetter = theHigherTheBetter;
+    public final String name;
+    private final EnumMap<StatisticKey,Integer> stats;
+
+    public User(String name, EnumMap<StatisticKey,Integer> stats) {
+        this.name = name;
+        this.stats = new EnumMap<StatisticKey,Integer>(stats);
+    }
+
+    public void setValue(StatisticKey key, int value) {
+        synchronized (this) {
+            stats.put(key, Integer.valueOf(value));
+        }
+        Main.getDatabaseManager().updateStats(this, key, value);
+    }
+
+    public void increaseValue(StatisticKey key, int amount) {
+        this.setValue(key, this.getValue(key) + amount);
+    }
+
+    public void updateValues(Map<StatisticKey,Integer> newValues) {
+        synchronized (this) {
+            stats.putAll(newValues);
+        }
+    }
+
+    public synchronized int getValue(StatisticKey key) {
+        final Integer cachedValue = stats.get(key);
+        return cachedValue != null ? cachedValue.intValue() : 0;
     }
 
 }
