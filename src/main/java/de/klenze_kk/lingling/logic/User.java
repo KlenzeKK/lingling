54cf1466959dcd83dbbf28774e29275ba97cf78f
KlenzeKK <57729531+KlenzeKK@users.noreply.github.com>
2020-05-03 00:00:06 +0200

    Update User.java

--- src/main/java/de/klenze_kk/lingling/logic/User.java
+++ src/main/java/de/klenze_kk/lingling/logic/User.java
@@ -1,6 +1,9 @@
 package de.klenze_kk.lingling.logic;
 
 import java.util.EnumMap;
+import java.util.Map;
+
+import de.klenze_kk.lingling.Main;
 
 public final class User {
 
@@ -13,10 +16,23 @@ public final class User {
     }
 
     public void setValue(StatisticKey key, int value) {
-        stats.put(key, Integer.valueOf(value));
+        synchronized (this) {
+            stats.put(key, Integer.valueOf(value));
+        }
+        Main.getDatabaseManager().updateStats(this, key, value);
     }
 
-    public int getValue(StatisticKey key) {
+    public void increaseValue(StatisticKey key, int amount) {
+        this.setValue(key, this.getValue(key) + amount);
+    }
+
+    public void updateValues(Map<StatisticKey,Integer> newValues) {
+        synchronized (this) {
+            stats.putAll(newValues);
+        }
+    }
+
+    public synchronized int getValue(StatisticKey key) {
         final Integer cachedValue = stats.get(key);
         return cachedValue != null ? cachedValue.intValue() : 0;
     }
